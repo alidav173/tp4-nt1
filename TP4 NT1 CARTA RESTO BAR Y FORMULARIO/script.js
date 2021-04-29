0626e540e8baf00ca882c3412394d702e74b7a5b
alidav173 <55164581+alidav173@users.noreply.github.com>
2021-04-28 22:44:49 -0300

    Update script.js
    nueva funcion mifuncionDos

--- TP4 NT1 CARTA RESTO BAR Y FORMULARIO/script.js
+++ TP4 NT1 CARTA RESTO BAR Y FORMULARIO/script.js
@@ -6,7 +6,7 @@
       var primerElem="";
       var i;
       for (i = 0; i < x.length ;i++) {
-        primerElem=x.elements[0].value
+        primerElem=x.elements[0].value;
         text += x.elements[i].value + " " + " ";
       }
       if ( primerElem ==="") {
@@ -18,5 +18,30 @@
          alert ("Se ha ingresado todos los datos satisfactoriamente: " + text);
 
      }
+
+    }
+
+    function miFuncionDos(){
+      var x = document.querySelectorAll(".tm-page-ali");
+      var textDos = "";
+      var primerElem="";
+      var i;
+        for (i = 0; i < x.length; i++) {
+          primerElem = x[0].value;
+          if(x[i].value ===""){
+            x[i].style.backgroundColor = "red";
+            alert ("Ingrese los datos solicitados");
+          }
+          else{
+            textDos += x[i].value + " " + " ";
+          }
+        }
+        if(primerElem ===""){
+          alert ("No se ha podido realizar el registro");
+        }
+        else{
+          alert ("Se ha ingresado todos los datos satisfactoriamente: " + textDos);
+        }
+
 
     }
